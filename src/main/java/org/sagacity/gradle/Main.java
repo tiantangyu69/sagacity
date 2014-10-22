8a4eb164805b7ca208be32cacbf0f52fe91d2e76
lizhitao <lizhitao123@126.com>
2014-10-22 15:15:20 +0800

    初始化

---
+++ src/main/java/org/sagacity/gradle/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.sagacity.gradle;
+
+/**
+ * @author LIZHITAO
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("this is the first gradle project");
+
+	}
+
+}
